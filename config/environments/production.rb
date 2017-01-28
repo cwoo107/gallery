8ff36ed8852d8a3edefb0521cd91e6da3a88e278
cwoo107 <calebwoods@calebs-mbp.gateway.sonic.net>
2017-01-28 14:51:09 -0800

    update production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -84,7 +84,9 @@ config.paperclip_defaults = {
     :bucket => ENV['S3_BUCKET_NAME'],
     :access_key_id => ENV['AWS_ACCESS_KEY_ID'],
     :secret_access_key => ENV['AWS_SECRET_ACCESS_KEY'],
-    s3_region: "us-west-1"
+    s3_region: "us-west-1",
+    :s3_host_name "s3-us-west-1.amazonaws.com"
+
   }
 }
 end
