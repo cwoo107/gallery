9b2371852b422d6a770e53ba0c7b4db5bfe1d1e3
cwoo107 <calebwoods@calebs-mbp.gateway.sonic.net>
2017-01-28 13:43:08 -0800

    update routes

--- app/models/picture.rb
+++ app/models/picture.rb
@@ -3,7 +3,14 @@ class Picture < ActiveRecord::Base
 	 :styles => {
    	 :thumb => "500x500#" },
   	 :convert_options => {
-     :thumb => "-quality 75 -strip" }
+     :thumb => "-quality 75 -strip" },
+     storage: => :s3,
+  	s3_credentials: {
+    bucket: => ENV['AWS_S3_BUCKET'],
+    access_key_id: =>ENV['AWS_ACCESS_KEY_ID'],
+    secret_access_key: =>ENV['AWS_SECRET_ACCESS_KEY'],
+    s3_region: =>ENV['AWS_REGION']
+	}
 	do_not_validate_attachment_file_type :image
 
 
